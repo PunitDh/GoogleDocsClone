0480cc89db576c94c3b3a3a22ae779ff24bfabcb
PunitDh <cas012115@coderacademy.edu.au>
2022-04-16 01:03:18 +1000

    Testing production build

--- server.js
+++ server.js
@@ -1,9 +1,47 @@
+const isProduction = process.env.NODE_ENV === "production";
 require("dotenv").config();
+const path = require("path");
 const mongoose = require("mongoose");
 const Document = require("./Document");
-const isProduction = process.env.NODE_ENV === "production";
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http").Server(app);
+const port = process.env.PORT || 3010;
+
+console.log({ isProduction });
+
+const io = require("socket.io")(
+  isProduction
+    ? http
+    : (process.env.PORT || 3001,
+      {
+        cors: {
+          origin: process.env.FRONTEND_URL,
+          methods: ["GET", "POST"],
+        },
+      })
+);
+
+isProduction && app.use(cors());
+isProduction && app.use(express.static(__dirname));
 
 console.log("Server starting...");
+
+isProduction &&
+  app.get("/", (_, res) => {
+    res
+      .status(200)
+      .sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+
+isProduction &&
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+isProduction &&
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 
 mongoose
   .connect(process.env.MONGODB_URL)
@@ -13,18 +51,6 @@ mongoose
   .catch((err) => {
     console.log("Failed to connect to MongoDB database", err);
   });
-
-const io = require("socket.io")(process.env.PORT || 3001, {
-  cors: {
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"],
-  },
-});
-
-// isProduction &&
-//   app.get("*", (_, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-//   });
 
 const defaultValue = "";
 
@@ -66,3 +92,6 @@ async function findOrCreateDocument(id) {
   if (document) return document;
   return await Document.create({ _id: id, data: defaultValue });
 }
+
+isProduction &&
+  http.listen(port, () => console.log("Server started on port", port));
