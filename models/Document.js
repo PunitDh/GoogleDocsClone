2bcce03f7a06520e88202a3efe1dc5c420195d28
PunitDh <cas012115@coderacademy.edu.au>
2022-04-17 00:31:27 +1000

    Add new models and update existing models

--- models/Document.js
+++ models/Document.js
@@ -3,8 +3,11 @@ const { Schema, model } = require("mongoose");
 const Document = new Schema(
   {
     _id: String,
-    title: String,
+    title: { type: String, required: true },
     data: Object,
+    visible: { type: Boolean, required: true, default: true },
+    userId: { type: String, required: true },
+    public: { type: Boolean, required: true, default: false },
   },
   { timestamps: true }
 );
--- models/User.js
+++ models/User.js
@@ -2,10 +2,27 @@ const { Schema, model } = require("mongoose");
 
 const User = new Schema(
   {
-    email: String,
-    password: String,
-    firstName: String,
-    lastName: String,
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    superUser: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
   { timestamps: true }
 );
